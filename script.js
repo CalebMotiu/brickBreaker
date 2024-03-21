544e470b225ea59ec7b4ae2b3841d5c40485f769
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-03-21 14:04:52 +0200

    function for r2d2

--- script.js
+++ script.js
@@ -7,40 +7,7 @@ function draw(){
     background("grey")
     
     smilyFaceCaleb()
-
-
-    //shape
-    fill("silver");
-    circle(100,100,40);
-    fill("white");
-    quad(80,100,120,100,120,150,80,150);
-    quad(70,100,80,100,80,155,70,155);
-    quad(120,100,130,100,130,155,120,155);
-    quad(70,155,80,155,82,162,68,162);
-    quad(120,155,130,155,132,162,118,162);
-    quad(85,150,115,150,110,155,90,155);
-    quad(95,155,105,155,110,160,90,160);
-    //head
-    fill("blue")
-    stroke("blue")
-    arc(100, 85,23,9,PI,TWO_PI)
-    rect(81,93,38,8,6,6,0,0)
-    rect(97,87,8,7)
-    fill("black")
-    circle(100,90,6)
-    fill("grey")
-    circle(110,97,7)
-    //body
-    fill("blue")
-    rect(90,103,22,2)
-    rect(90,107,22,2)
-    rect(90,111,22,2)
-    rect(97,117,6,12)
-    fill("grey")
-    stroke("grey")
-    rect(98,118,4,4,1,)
-    rect(98,124,4,4,1,)
-
+    r2d2()
 
 }
 function smilyFaceCaleb(){
@@ -60,4 +27,38 @@ function smilyFaceCaleb(){
     stroke("black")
     fill("red");
     arc(400,503,10,7,0,PI)
+}
+function r2d2(){
+        //shape
+        fill("silver");
+        circle(100,100,40);
+        fill("white");
+        quad(80,100,120,100,120,150,80,150);
+        quad(70,100,80,100,80,155,70,155);
+        quad(120,100,130,100,130,155,120,155);
+        quad(70,155,80,155,82,162,68,162);
+        quad(120,155,130,155,132,162,118,162);
+        quad(85,150,115,150,110,155,90,155);
+        quad(95,155,105,155,110,160,90,160);
+        //head
+        fill("blue")
+        stroke("blue")
+        arc(100, 85,23,9,PI,TWO_PI)
+        rect(81,93,38,8,6,6,0,0)
+        rect(97,87,8,7)
+        fill("black")
+        circle(100,90,6)
+        fill("grey")
+        circle(110,97,7)
+        //body
+        fill("blue")
+        rect(90,103,22,2)
+        rect(90,107,22,2)
+        rect(90,111,22,2)
+        rect(97,117,6,12)
+        fill("grey")
+        stroke("grey")
+        rect(98,118,4,4,1,)
+        rect(98,124,4,4,1,)
+    
 }
