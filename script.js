46f0d017d3826f3e56b0b1105f697a80ff40a370
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-03-21 12:36:07 +0200

    aded smilyFaceCaleb()

--- script.js
+++ script.js
@@ -3,5 +3,32 @@ function setup(){
 }
 
 function draw(){
+    //head
+    background("grey")
+    
+    smilyFaceCaleb()
 
+
+    //head
+    fill("white");
+    circle(100,100,40);
+    quad(80,100,120,100,120,150,80,150)
+}
+function smilyFaceCaleb(){
+    stroke("black");
+    fill("red");    
+    circle(400,500,20);
+    //eye
+    stroke("black");
+    circle(397,497,3);
+    circle(403,497,3);
+    stroke("blue");
+    fill("blue");
+    circle(397,497,2);
+    circle(403,497,2);
+
+    //mouth
+    stroke("black")
+    fill("red");
+    arc(400,503,10,7,0,PI)
 }
