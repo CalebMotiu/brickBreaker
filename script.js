f929938c4d3f7ec231055ea27a8f0d5f5a360dd2
username <email addres>
2024-03-28 11:53:10 +0200

    smilyFaceEzy

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ function setup(){
 function draw(){
     //head
     background("grey")
-    
-    smilyFaceCaleb()
-    r2d2()
+    smilyFaceEzy(400,300);
+    //smilyFaceCaleb()
+   
 
 }
 function smilyFaceCaleb(){
     stroke("black");
     fill("red");    
     circle(400,500,20);
+    
     //eye
     stroke("black");
     circle(397,497,3);
@@ -28,37 +29,20 @@ function smilyFaceCaleb(){
     fill("red");
     arc(400,503,10,7,0,PI)
 }
-function r2d2(){
-        //shape
-        fill("silver");
-        circle(100,100,40);
-        fill("white");
-        quad(80,100,120,100,120,150,80,150);
-        quad(70,100,80,100,80,155,70,155);
-        quad(120,100,130,100,130,155,120,155);
-        quad(70,155,80,155,82,162,68,162);
-        quad(120,155,130,155,132,162,118,162);
-        quad(85,150,115,150,110,155,90,155);
-        quad(95,155,105,155,110,160,90,160);
-        //head
-        fill("blue")
-        stroke("blue")
-        arc(100, 85,23,9,PI,TWO_PI)
-        rect(81,93,38,8,6,6,0,0)
-        rect(97,87,8,7)
-        fill("black")
-        circle(100,90,6)
-        fill("grey")
-        circle(110,97,7)
-        //body
-        fill("blue")
-        rect(90,103,22,2)
-        rect(90,107,22,2)
-        rect(90,111,22,2)
-        rect(97,117,6,12)
-        fill("grey")
-        stroke("grey")
-        rect(98,118,4,4,1,)
-        rect(98,124,4,4,1,)
+function smilyFaceEzy(x,y) {
+    background(220);
     
-}
+  
+    fill(255, 255, 0); 
+    ellipse(x, y, 50); 
+    
+    
+    fill(0); 
+    ellipse(x,y, 50); 
+    ellipse(x, y, 50); 
+    
+    noFill();
+    stroke(0); 
+    strokeWeight(5);
+    arc(200, 250, 100, 100, 0, PI); 
+  }
