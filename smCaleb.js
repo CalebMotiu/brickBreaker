826f29ecbe74c30f02bcb52db60f848722198572
Caleb Motiu <caleb.motiu@emanuel.ro>
2024-04-04 12:21:15 +0300

    only until border

--- smCaleb.js
+++ smCaleb.js
@@ -2,33 +2,35 @@
 let smCaleb={
 x : 100,
 y : 150,
+diameter:50,
 moveX:1,
 moveY:1,
-speed:100
+speedX:5,
+speedY:10
 };
 function verfyMovmentCaleb(){
-    if (smCaleb.x >= right || smCaleb.x <= leftAndTop)
+    if (smCaleb.x >= right-(smCaleb.diameter/2) || smCaleb.x <= leftAndTop+(smCaleb.diameter/2))
     {smCaleb.moveX *= -1;}
 
-    smCaleb.x += smCaleb.moveX * smCaleb.speed;
+    smCaleb.x += smCaleb.moveX * smCaleb.speedX;
 
-    if (smCaleb.y >= bottom || smCaleb.y <= leftAndTop)
+    if (smCaleb.y >= bottom-(smCaleb.diameter/2) || smCaleb.y <= leftAndTop+(smCaleb.diameter/2))
     {smCaleb.moveY *= -1;}
 
-    smCaleb.y += smCaleb.moveY* smCaleb.speed;
+    smCaleb.y += smCaleb.moveY* smCaleb.speedY;
 }
 function smilyFaceCaleb(x, y) {
     stroke("black");
     fill("red");
-    circle(x, y, 20);
+    circle(x, y, smCaleb.diameter);
     //eye
     stroke("black");
-    circle(x-3,y-3,3);
-    circle(x+3,y-3,3);
+    circle(x-3,y-3,5);
+    circle(x+3,y-3,5);
     stroke("blue");
     fill("blue");
-    circle(x-3,y-3,2);
-    circle(x+3,y-3,2);
+    circle(x-3,y-3,4);
+    circle(x+3,y-3,4);
 
     //mouth
     stroke("black")
