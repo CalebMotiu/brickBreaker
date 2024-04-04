83ab9673313e2639046e39357b09cdcad8665e38
username <email addres>
2024-04-04 11:44:05 +0300

    v10

--- script.js
+++ script.js
@@ -2,17 +2,6 @@ let leftAndTop=0;
 let right =800;
 let bottom = 600;
 
-<<<<<<< HEAD
-let calebPointX = 100;
-let calebPointY = 100;
-
-let calebMoveX=10;
-let ezyMoveX=300;
-let ezyMoveY=300;
-let directionEzyX=10;
-=======
-
->>>>>>> eddad47706050e2f2513669151e915e44f02255c
 function setup() {
     createCanvas(right, bottom);
 }
@@ -20,7 +9,7 @@ function setup() {
 function draw() {
     //head
     background("grey")
-    smilyFaceEzy(  ezyMoveX,ezyMoveY);
+    smilyFaceEzy(  ezyMove.x,ezyMove.y);
     smilyFaceDavid(davidMoveX, davidMoveY);
     
     smilyFaceCaleb(calebPointX, calebPointY)
@@ -31,53 +20,6 @@ function draw() {
 
 }
 
-<<<<<<< HEAD
-    calebPointX += calebMoveX;
-}
-function smilyFaceCaleb(x, y) {
-    stroke("black");
-    fill("red");
-    circle(x, y, 20);
-    //eye
-    stroke("black");
-    circle(x-3,y-3,3);
-    circle(x+3,y-3,3);
-    stroke("blue");
-    fill("blue");
-    circle(x-3,y-3,2);
-    circle(x+3,y-3,2);
-
-    //mouth
-    stroke("black")
-    fill("red");
-    arc(x,y+3,10,7,0,PI)
-}
-function verfyMovmentEzy(){
-    if (ezyMoveX >= right || ezyMoveX <= leftAndTop)
-    {directionEzyX *= -1;}
-
-    ezyMoveX += directionEzyX;
-}
-
-
-function smilyFaceEzy(x, y) {
-  
-    fill(255, 200, 0);
-    ellipse(x, y, 50);
-
-
-    fill(5,0,0,)
-    ellipse(x-3, y-10, 5);
-    ellipse(x+3, y-10, 5);
-
-    
-    stroke(0);
-    //strokeWeight(5);
-    arc(x, y, 20, 20, 0, PI);
-}
-
-=======
->>>>>>> eddad47706050e2f2513669151e915e44f02255c
 
 
 function r2d2() {
