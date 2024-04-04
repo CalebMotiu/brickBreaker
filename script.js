080574a36f9c380ca41f3a22938d8dd6cf6a7e34
username <email addres>
2024-04-04 10:37:26 +0300

    v 7

--- script.js
+++ script.js
@@ -13,9 +13,7 @@ let calebPointY = 150;
 let calebMoveX=10;
 let calebMoveY=10;
 
-let ezyMoveX=300;
-let ezyMoveY=300;
-let directionEzyX=10;
+
 function setup() {
     createCanvas(right, bottom);
 }
@@ -61,29 +59,7 @@ function smilyFaceCaleb(x, y) {
     fill("red");
     arc(x,y+3,10,7,0,PI)
 }
-function verfyMovmentEzy(){
-    if (ezyMoveX >= right || ezyMoveX <= leftAndTop)
-    {directionEzyX *= -1;}
 
-    ezyMoveX += directionEzyX;
-}
-
-
-function smilyFaceEzy(x, y) {
-  
-    fill(255, 200, 0);
-    ellipse(x, y, 50);
-
-
-    fill(5,0,0,)
-    ellipse(x-3, y-10, 5);
-    ellipse(x+3, y-10, 5);
-
-    
-    stroke(0);
-    //strokeWeight(5);
-    arc(x, y, 20, 20, 0, PI);
-}
 
 function checkLimitsSMDavid(){
     if (davidMoveX >= right || davidMoveX <= leftAndTop)
